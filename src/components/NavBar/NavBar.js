c727c552d219c1cfa026b06f5dc3d62efb42a7ad
Pablo Correa <bronskins@hotmail.com>
2021-08-14 20:57:38 -0300

    Adding files

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+export const NavBar = () => {
+
+    return (
+
+        <header className="encabezado">
+
+            <h1>LOGO</h1>
+
+            <nav>
+                <a href="#">Nosotros</a>
+                <a href="#">Contacto</a>
+                <a href="#">Novedades</a>
+            </nav>
+
+        </header>
+
+    )
+
+}
